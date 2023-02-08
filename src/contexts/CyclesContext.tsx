cc1699295ee6a6a3dd0019641986f03bcfcc09b7
Sanarielsen <sanarielsen.dev@gmail.com>
2023-02-07 23:46:37 -0300

    feature: Organize Countdown and send variables at Context

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -14,7 +14,10 @@ interface CyclesContextType {
   cycles: Cycle[],
   activeCycle: Cycle | undefined,
   activeCycleId: string | null,
+  totalSeconds: number,
   amountSecondsPassed: number,
+  minutes: String,
+  seconds: String,
   markCurrentCycleAsFinished: () => void,
   setSecondsPassed: (seconds: number) => void,
   createNewCycle: (data: CreateCycleData) => void,
@@ -54,6 +57,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
   const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
+  const [ haveFinishedCycle, setHaveFinishedCycle ] = useState(false);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
     if (activeCycle) {
 
@@ -66,11 +70,59 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
     return 0
   })
 
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+  const minutes = String(minutesAmount).padStart(2, '0')
+  const seconds = String(secondsAmount).padStart(2, '0')
+
   useEffect(() => {
     const stateJSON = JSON.stringify(cyclesState)
 
     localStorage.setItem('@Ignite-Timer:cyclesState', stateJSON)
   }, [cyclesState])  
+
+  useEffect(() => {
+
+    if (activeCycle)
+      document.title = `Ignite Timer - ${minutes}:${seconds}`
+    else if (haveFinishedCycle && minutesAmount === 0 && secondsAmount === 0 )
+      document.title = `Ignite Timer - Finished!`
+  }, [minutes, seconds])
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(), 
+          new Date(activeCycle.startDate)
+        )
+        
+        if (secondsDifference >= totalSeconds) {
+          markCurrentCycleAsFinished()
+          setSecondsPassed(totalSeconds)
+          setHaveFinishedCycle(true)
+          clearInterval(interval)
+        } else {
+          setSecondsPassed(secondsDifference)
+        }                          
+      }, 1000)
+    }
+
+    return () => {
+
+      clearInterval(interval)
+    }
+  }, [activeCycle, 
+    totalSeconds, 
+    activeCycleId, 
+    markCurrentCycleAsFinished, 
+    setSecondsPassed
+  ]);
 
   function markCurrentCycleAsFinished() {
 
@@ -113,7 +165,10 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
         cycles,
         activeCycle,
         activeCycleId,
-        amountSecondsPassed,        
+        totalSeconds,
+        amountSecondsPassed,
+        minutes,
+        seconds,
         markCurrentCycleAsFinished,
         setSecondsPassed,
         createNewCycle,
