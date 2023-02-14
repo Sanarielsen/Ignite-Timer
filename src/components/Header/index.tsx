72e85783d4c1112257149649b2bd4cdacb20edd6
Sanarielsen <sanarielsen.dev@gmail.com>
2023-02-14 00:47:22 -0300

    Image Title of Site Fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,6 @@ import { Timer, Scroll } from 'phosphor-react'
 import { HeaderContainer } from "./styles";
 import logoIgnite from '../../assets/logo-ignite.svg';
 
-
 export function Header() {
 
     return (
