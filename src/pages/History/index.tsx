9d4fc7a86c34aaf25a05191550b473f417959ea0
Sanarielsen <sanarielsen.dev@gmail.com>
2023-02-20 23:29:52 -0300

    feat: Added Scroll Infinite in a history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 
 import { CyclesContext } from "../../contexts/CyclesContext";
 import { 
@@ -13,6 +13,7 @@ import {
 import { ModalConfirmation } from "../../components/Modal/ModalConfirmation";
 import { ModalStructure } from "../../components/Modal/ModalStructure";
 import { HistoryData } from "./components/HistoryData";
+import { Cycle } from "../../reducers/cycles/reducer";
 
 const headerHistoryList = [
 
@@ -26,15 +27,36 @@ export function History() {
   const { cycles, deleteListOfCycles } = useContext(CyclesContext)  
   const ref = useRef<HTMLButtonElement>(null);
 
-  const [openModalConfirm, setOpenModalConfirm] = useState<boolean>(false);
+  let elementsPerLoad = 10;
+  
+  const [quantCyclesLoaded, setQuantCyclesLoaded] = useState(elementsPerLoad);
+  const [cyclesLoaded, setCyclesLoaded] = useState<Cycle[]>(cycles.slice(0, quantCyclesLoaded))
+  const [currentPage, setCurrentPage] = useState(1)
+  const [openModalConfirm, setOpenModalConfirm] = useState<boolean>(false);  
 
-  const haveCycles = cycles.length;
+  const haveCycles = cycles.length;  
   
   function handleCyclesRemove() {
 
     setOpenModalConfirm(false)
     deleteListOfCycles()      
   }
+
+  useEffect(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+      if (entries.some((entry) => entry.isIntersecting))      
+        setCurrentPage((currentPageInsideState) => currentPageInsideState + 1);
+    });
+    intersectionObserver.observe(document.querySelector('#panelReference') as HTMLElement)
+    return () => intersectionObserver.disconnect();
+  }, [])
+
+  useEffect(() => {
+    if (cyclesLoaded) {      
+      setQuantCyclesLoaded(quantCyclesLoaded + elementsPerLoad)
+      setCyclesLoaded((current) => current = cycles.slice(0, quantCyclesLoaded))      
+    }      
+  }, [currentPage])
 
   return (
     
@@ -59,7 +81,8 @@ export function History() {
         </ModalStructure>
       </HistoryContainerHeader>      
       
-      <HistoryList>
+      <h1> Pagina atual: {currentPage} </h1>
+      <HistoryList>        
         <HistoryTable>
           <thead>
             <tr>
@@ -71,14 +94,14 @@ export function History() {
             </tr>
           </thead>
           <HistoryTableBody>            
-            {cycles.map((cycle) => {
+            {cyclesLoaded.map((cycle) => {
               return (
                 <HistoryData key={cycle.id} cycle={cycle} />
               )
             })}
-            <tr id="panelReference" style={{width: "100%", height: "5px", backgroundColor: "red"}}> Teste </tr>             
-          </HistoryTableBody>  
-        </HistoryTable>
+            <div id="panelReference" style={{width: "100%", height: "5px"}}></div>   
+          </HistoryTableBody>            
+        </HistoryTable>        
       </HistoryList>
     </HistoryContainer>
   )
